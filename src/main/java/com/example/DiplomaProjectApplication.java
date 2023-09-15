4bb26399a1fb472908795c74e01f0373db844358
yulia-l <yulia.lyugunaya@yandex.ru>
2023-09-15 14:18:48 +0300

    Initial commit

---
+++ src/main/java/com/example/DiplomaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiplomaProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiplomaProjectApplication.class, args);
+    }
+
+}
